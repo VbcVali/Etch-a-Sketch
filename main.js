23a45243a5e671c593f76b02d0ecaf844f3a44d3
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-05-18 21:56:05 +0200

    First implementation of grid in js and style.ç

---
+++ main.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+const reset = document.querySelector(".reset");
+
+function newGrid(gridS) {
+    const cellSize = 100 / gridS;
+    for (let i = 0; i < gridS * gridS; i++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        container.appendChild(div);
+        div.style.flex = `1 0 ${cellSize}%`;
+    };
+    
+};
+
+grid.addEventListener("click", () => {
+    let gridSize = prompt("Grid size ? ");
+    gridSize = parseInt(gridSize);
+
+    if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+        alert("Please enter a valid number.")
+        gridSize = 16;
+    }
+    newGrid(gridSize);
+});
+
+reset.addEventListener("click", () => {
+    container.innerHTML = "";
+});
